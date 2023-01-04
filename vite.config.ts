a84000ad8aaa654482ed1ab832d862781e2e946b
Samson Okunola <smsnmicheal@gmail.com>
2023-01-04 16:33:16 +0100

    config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [react(), tsconfigPaths()],
-})
+    plugins: [react(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+    },
+  },
+  server: {
+    hmr: {
+      overlay: false,
+    },
+  },
+});
